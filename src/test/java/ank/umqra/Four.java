0927259a6846a697cee2ce2271d3b827df067914
42ann <ka.nippon@gmail.com>
2022-08-23 21:21:57 +0300

    бефоре, афтер и прочие радости тестерской жизни

--- src/test/java/ank/umqra/Four.java
+++ src/test/java/ank/umqra/Four.java
@@ -6,28 +6,32 @@ import org.junit.jupiter.api.*;
 
 public class Four {
 
-    @BeforeAll
+    @BeforeAll//выполнять перед всем скоупом
     static void initDB() {
-        System.out.println("### @BeforeEach");
-        Selenide.open();
-    }//бефоре алл всегда статик
+        System.out.println("### @BeforeAll");
+    }//бефореAлл всегда статик. инитДБ неведомая хрень, погуглить
 
-    @BeforeEach //выполнять перед каждым тестом
+    @BeforeEach//выполнять перед каждым тестом
     void openGo(){
+        System.out.println("### @BeforeEach");
         Selenide.open("https://google.org");
-        }
+    }
     @AfterEach //выполнять после каждого кейса
     void closeGo(){
+        System.out.println("### @AfterEach");
         WebDriverRunner.closeWindow();
     }
-
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### @AfterAll");
+    }//афтер алл всегда статик. клинДБ хрень полуведомая, вроде к начальному состоянию приводит. погуглить
     @Test
-    void assertTest(){
-       Assertions.assertTrue(1 > 5);//проверяет, верно ли выражение
+        void assertTest(){
+       Assertions.assertTrue(1 > 0);//проверяет, верно ли выражение
     }
 
     @Test
-    void assertTest2(){
+        void assertTest2(){
         Assertions.assertTrue(1 < 5);
     }
 
